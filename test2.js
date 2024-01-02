d18fe9607cfd817204769cdd049011bd72ac262a
Juan Machaca <149337018+Juan6142@users.noreply.github.com>
2024-01-02 09:57:54 -0500

    Added UI with DOM Methods and JS code Modified

--- test2.js
+++ test2.js
@@ -12,49 +12,128 @@ function getComputerChoice() {
 function round(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     if (playerSelection === computerSelection) {
+        roundWinner.textContent = "Tie!"
         return "Tie!"
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        roundWinner.textContent = "You win!"
         return "You win";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        roundWinner.textContent = "You win!"
         return "You win";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        roundWinner.textContent = "You win!"
         return "You win";
     } else {
+        roundWinner.textContent = "You lose!"
         return "You lose";
     }
 }
 
-// funcion de juego de 5 rondas
+// game
+let pointsToWin = 5
+let playerWins = 0
+let computerWins = 0
+
 function game() {
-    let playerWins = 0
-    let computerWins = 0
-    for (let x = 1; x < 6; x++) {
-        const playerSelection = prompt("Choose between rock, paper or scissors.")
-        const computerSelection = getComputerChoice() //llamada a funcion getcomputerchoice
-        const result = round(playerSelection, computerSelection); // llamada a funcion ronda
-        
-        console.log(`Player Selection is: ${playerSelection}`)
-        console.log(`Computer Selection is: ${computerSelection}`)
-        console.log(`Round ${x}: ${result}`)
+    //const playerSelection = prompt("Choose between rock, paper or scissors.")
+    let computerSelection = getComputerChoice() //llamada a funcion getcomputerchoice
+    spanSecond.textContent = computerSelection
+    let result = round(playerSelection, computerSelection); // llamada a funcion ronda
 
-        if (result === "You win") {
-            playerWins++
-        } else if (result === "You lose") {
-            computerWins++
-        } 
+    console.log(`Player Selection is: ${playerSelection}`)
+    console.log(`Computer Selection is: ${computerSelection}`)
+    console.log(`${result}`)
+   
+    if (result === "You win") {
+        playerWins++
+    } else if (result === "You lose") {
+        computerWins++
+    } 
         console.log(`Player score: ${playerWins} - Computer score: ${computerWins}`);
-    }
-    if (playerWins > computerWins) {
-        console.log("You win the game!"); 
-    } else if (playerWins < computerWins) {
-        console.log("You lose the game!"); 
-    } else {
-        console.log("Its a tie!");
-    }
-}
-//llamada a funcion game para ejecutar el programa
-game();
+
+    if (playerWins == 5) {
+        console.log("You win the game!");
+        resultSpan.style.color = 'green' 
+        restart.appendChild(btnRestart)
+        btnRestart.setAttribute("class", "restart-btn")
+        btnRestart.setAttribute("type", "button")
+        btnRestart.textContent = "PLAY AGAIN?"
+        return resultSpan.textContent = "YOU WIN THE GAME"
+
+    } else if (computerWins == 5) { 
+        console.log("You lose the game!");
+        resultSpan.style.color = 'red'
+        restart.appendChild(btnRestart)
+        btnRestart.setAttribute("class", "restart-btn")
+        btnRestart.setAttribute("type", "button")
+        btnRestart.textContent = "PLAY AGAIN?" 
+        return resultSpan.textContent = "YOU LOSE THE GAME"
+    
+    } 
+    resultSpan.textContent = `Player: ${playerWins}` + ` - ` + `Computer: ${computerWins}`
+} 
 
 
-/* falta entender ampliamente el funcionanmiento de la consola y la llamada de funciones y funcionamiento de parametros, estudiar
-mas los metodos de strings, analizar el problema no aplicaste el metodo que aprendiste*/
+const btnRock = document.querySelector(".btn-rock")
+const btnPaper = document.querySelector(".btn-paper")
+const btnScissors = document.querySelector(".btn-scissors")
+let playerSelection;
+
+btnRock.addEventListener("click", () => {
+    playerSelection = "rock"
+    game()
+    spanFirst.textContent = playerSelection
+});
+
+btnPaper.addEventListener("click", () => {
+    playerSelection = "paper"
+    game()
+    spanFirst.textContent = playerSelection
+});
+
+btnScissors.addEventListener("click", () => {
+    playerSelection = "scissors"
+    game()
+    spanFirst.textContent = playerSelection
+});
+
+
+// seleccion
+
+const h3First = document.querySelector(".h3-user-selection");
+const spanFirst = document.createElement("span");
+spanFirst.style.color = "green"
+h3First.appendChild(spanFirst)
+
+const h3Second =document.querySelector(".h3-computer-selection")
+const spanSecond = document.createElement("span")
+spanSecond.style.color = "red"
+h3Second.appendChild(spanSecond)
+
+//resultados
+const roundWinner = document.querySelector(".round-winner")
+
+const currentScore = document.querySelector(".current-score")
+const resultSpan = document.createElement("span")
+
+currentScore.appendChild(resultSpan)
+
+//restart
+const btnRestart = document.createElement("button")
+const restart = document.querySelector(".score")
+
+
+
+btnRestart.addEventListener("click", () => {
+    playerWins = 0
+    computerWins = 0
+    roundWinner.textContent = ""
+    resultSpan.textContent = ""
+    spanFirst.textContent = ""
+    spanSecond.textContent = ""
+    resultSpan.style.color = 'black'
+    restart.removeChild(btnRestart) 
+});
+
+// no se puede remover el evento ya que estoy usando funciones sin nombre y el metodo para elimnar
+//requiere que se use, fuera de esa esta todo completo creo yo,
